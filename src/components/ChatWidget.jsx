ea813899370ae1e421448e3ac98d2b149a60e2a6
Kaushik Ramabhotla <ramabhotlakaushik@gmail.com>
2026-05-28 22:32:44 +0530

    friend-request-8

--- src/components/ChatWidget.jsx
+++ src/components/ChatWidget.jsx
@@ -58,7 +58,7 @@ function ChatWidget() {
     console.log(request.id);
     console.log(request.senderId);
     await axios.post(
-      `${import.meta.env.VITE_API_URL}/api/users/accept?senderId=${request.id}`,
+      `${import.meta.env.VITE_API_URL}/api/users/accept?requestId=${request.id}`,
       {},
       {
         headers: {
